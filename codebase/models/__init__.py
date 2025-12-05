3ec1bde3554ae1097218d628541135b2c7f9d9cc
Sheng <demonicplayer404@gmail.com>
2025-12-04 22:58:34 -0500

    ResNet

--- codebase/models/__init__.py
+++ codebase/models/__init__.py
@@ -1,15 +1,11 @@
 #Export all models for easy importing
 
 from .base_model import BaseModel
-from .neural_networks import SimpleCNN, ResNetModel, TransformerModel
+from .neural_networks import ResNetModel
 
 #Factory pattern for creating models
 def create_model(model_type, config):
-    if model_type == 'cnn':
-        return SimpleCNN(config)
-    elif model_type == 'resnet':
+    if model_type == 'resnet':
         return ResNetModel(config)
-    elif model_type == 'transformer':
-        return TransformerModel(config)
     else:
-        raise ValueError(f"Unknown model type: {model_type}")
+        raise ValueError(f"Unknown model type: {model_type}. Only 'resnet' is supported.")
